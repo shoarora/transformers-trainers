c32754f910370fa9b1a830db99d8aafb35308682
Sho Arora <shoarora@mercari.com>
2020-03-07 00:21:59 -0800

    Write forward

--- lmtuners/lightning_modules/glue.py
+++ lmtuners/lightning_modules/glue.py
@@ -11,6 +11,8 @@ from transformers import glue_convert_examples_to_features as convert_examples_t
 
 logger = logging.getLogger(__name__)
 
+MODEL_CLASSES = {}  # TODO
+
 
 class GlueModuleConfig(Namespace):
     def __init__(self):
@@ -18,12 +20,101 @@ class GlueModuleConfig(Namespace):
 
 
 class GlueModule(pl.LightningModule):
-    def __init__(self, model, tokenizer, config):
-        pass
+    def __init__(self, config):
+        self.hparams = config
+        self.config = config
 
-    def _get_dataloader(self, mode):
-        task = self.config.task
+        self._init_model()
 
+    def _init_model(self):
+        self.config.model_type = self.config.model_type.lower()
+
+        config_class, model_class, tokenizer_class = MODEL_CLASSES[
+            self.config.model_type]
+
+        if self.config.tokenizer_type:
+            _, _, tokenizer_class = MODEL_CLASSES[
+                self.config.tokenizer_type.lower()]
+
+        processor = processors[self.config.task_name]()
+        label_list = processor.get_labels()
+        self.config.model_type = len(label_list)
+
+        config = config_class.from_pretrained(
+            self.config.config_name
+            if self.config.config_name else self.config.model_name_or_path,
+            self.config.model_type=self.config.model_type,
+            finetuning_task=self.config.task_name,
+            cache_dir=self.config.cache_dir if self.config.cache_dir else None,
+        )
+        self.tokenizer = tokenizer_class.from_pretrained(
+            self.config.tokenizer_name
+            if self.config.tokenizer_name else self.config.model_name_or_path,
+            do_lower_case=self.config.do_lower_case,
+            cache_dir=self.config.cache_dir if self.config.cache_dir else None,
+        )
+        self.model = model_class.from_pretrained(
+            self.config.model_name_or_path,
+            from_tf=bool(".ckpt" in self.config.model_name_or_path),
+            config=config,
+            cache_dir=self.config.cache_dir if self.config.cache_dir else None,
+        )
+
+    def forward(self, inputs):
+        outputs = self.model(**inputs)
+        return outputs
+
+    def training_step(self, batch):
+        inputs = {
+            "input_ids": batch[0],
+            "attention_mask": batch[1],
+            "labels": batch[3]
+        }
+        if self.config.model_type != "distilbert":
+            inputs["token_type_ids"] = (
+                batch[2] if
+                self.config.model_type in ["bert", "xlnet", "albert"] else None
+            )  # XLM, DistilBERT, RoBERTa, and XLM-RoBERTa don't use segment_ids
+
+        outputs = self.forward(inputs)
+        loss = outputs[0]
+        tensorboard_logs = {'train_loss': loss}
+        return {'loss': loss, 'log': tensorboard_logs}
+
+    def validation_step(self, batch):
+        inputs = {"input_ids": batch[0], "attention_mask": batch[1], "labels": batch[3]}
+        if args.model_type != "distilbert":
+            inputs["token_type_ids"] = (
+                batch[2] if args.model_type in ["bert", "xlnet", "albert"] else None
+            )  # XLM, DistilBERT, RoBERTa, and XLM-RoBERTa don't use segment_ids
+        outputs = model(**inputs)
+        eval_loss, logits = outputs[:2]
+        return {
+            'val_loss': eval_loss,
+            'logits': logits,
+            'labels': inputs['labels']
+        }
+
+    def validation_end(self, outputs):
+        avg_val_loss = torch.stack([o['val_loss'] for o in outputs]).mean()
+        logits = torch.stack([o['logits'] for o in outputs])
+        labels = torch.stack([o['labels'] for o in outputs])
+
+        if self.output_mode == 'classification':
+            preds = logits.argmax(dim=-1)
+        elif self.output_mode == 'regression':
+            preds = logits.squeeze()
+
+        preds = preds.detach().cpu().numpy()
+        labels = labels.detach().cpu().numpy()
+
+        result = compute_metrics(self.config.task_name, preds, labels)
+
+        return {
+            'tensorboard_logs': result, 'avg_val_loss': avg_val_loss
+        }
+
+    def _get_dataloader(self, task, mode):
         processor = processors[task]()
         output_mode = output_modes[task]
         if mode == 'train':
@@ -70,15 +161,18 @@ class GlueModule(pl.LightningModule):
                                                  all_attention_mask,
                                                  all_token_type_ids,
                                                  all_labels)
-        return dataset
+        loader = torch.utils.data.DataLoader(dataset, shuffle=(mode == 'train'), batch_size=self.config.batch_size)
+        return loader
 
     @pl.data_loader
     def train_dataloader(self):
-        return self._get_dataloader('train')
+        return self._get_dataloader(self.config.task_name, 'train')
 
     @pl.data_loader
     def val_dataloader(self):
-        return self._get_dataloader('dev')
+        eval_task_names = ("mnli", "mnli-mm") if self.config.task_name == "mnli" else (self.config.task_name,)
+        loaders = [self._get_dataloader(task, 'dev') for task in eval_task_names]
+        return loaders
 
     @pl.data_loader
     def test_dataloader(self):
