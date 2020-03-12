29ba69f4636af45c98bd89e4e8ca5516bc03478a
Sho Arora <shoarora@mercari.com>
2020-03-11 17:23:22 -0700

    Save the base models only

--- experiments/disc_lm_small/train_electra_small.py
+++ experiments/disc_lm_small/train_electra_small.py
@@ -96,7 +96,7 @@ def main(tokenizer_path,
     # save the model.
     output_path = os.path.join(save_path, 'discriminator', 'final')
     os.makedirs(output_path, exist_ok=True)
-    lightning_module.discriminator.save_pretrained(output_path)
+    lightning_module.discriminator.base_model.save_pretrained(output_path)
     if checkpoint_fn:
         checkpoint_fn(lightning_module)
 
