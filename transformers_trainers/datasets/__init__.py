1993b076b3e22bedbd3862266bc87a076f610360
Sho Arora <shoarora@mercari.com>
2020-08-11 16:19:03 -0400

    Update dataset types

--- transformers_trainers/datasets/__init__.py
+++ transformers_trainers/datasets/__init__.py
@@ -1,5 +1,4 @@
-from .line_by_line import (LineByLineCollater, LineByLineDataset,  # noqa: F401
-                           create_line_by_line_dataset)
-from .pretokenized import (PreTokenizedCollater,  # noqa: F401
-                           PreTokenizedFileDataset,
-                           create_pretokenized_dataset)
+from .line_by_line import LineByLineTextDataset
+
+
+__all__ = [LineByLineTextDataset]
--- transformers_trainers/datasets/line_by_line.py
+++ transformers_trainers/datasets/line_by_line.py
@@ -1,83 +1,38 @@
+from torch.utils.data import Dataset
+from transformers import PreTrainedTokenizer
+import os
 import torch
-from transformers_trainers.utils import mask_tokens
-from torch.nn.utils.rnn import pad_sequence
-from torch.utils.data import ConcatDataset, Dataset
 
 
-class LineByLineCollater():
-    def __init__(self,
-                 tokenizer,
-                 mlm=True,
-                 mlm_prob=0.15,
-                 pad_token_id=None,
-                 mask_token_id=None,
-                 vocab_size=None,
-                 cls_token_id=None):
+class LineByLineTextDataset(Dataset):
+    """ Based on huggingface/transformers v3.0.0 implementation.
+    This will be superseded by a framework-agnostic approach
+    soon.
+    """
+
+    def __init__(self, tokenizer: PreTrainedTokenizer, file_path: str, block_size: int):
+        assert os.path.isfile(file_path)
+        # Here, we do not cache the features, operating under the assumption
+        # that we will soon use fast multithreaded tokenizers from the
+        # `tokenizers` repo everywhere =)
+
+        with open(file_path, encoding="utf-8") as f:
+            self.lines = [
+                line
+                for line in f.read().splitlines()
+                if (len(line) > 0 and not line.isspace())
+            ]
         self.tokenizer = tokenizer
-        self.mlm = mlm
-        self.mlm_prob = mlm_prob
-        self.pad_token_id = pad_token_id
-        self.mask_token_id = mask_token_id
-        self.vocab_size = vocab_size
-        self.cls_token_id = None
-
-    def __call__(self, examples):
-        # TODO how to make this backward compatible with the old tokenizers?
-        encodings = self.tokenizer.encode_batch(examples)
-        ids = []
-        attention_masks = []
-        special_tokens_masks = []
-        for e in encodings:
-            ids.append(e.ids)
-            attention_masks.append(e.attention_mask)
-            special_tokens_masks.append(e.special_tokens_mask)
-
-        inputs = torch.tensor(ids, dtype=torch.long)
-        attention_masks = torch.tensor(attention_masks,
-                                       dtype=torch.long)
-        special_tokens_masks = torch.tensor(special_tokens_masks,
-                                            dtype=torch.bool)
-
-        if self.mlm:
-            inputs, labels = mask_tokens(inputs, special_tokens_masks,
-                                         self.pad_token_id, self.mask_token_id,
-                                         self.vocab_size, self.mlm_prob)
-            return inputs, labels, attention_masks
-        else:
-            return inputs, inputs, attention_masks
-
-    def _pad_sequence(self, inputs):
-        if self.pad_token_id is None:
-            inputs = pad_sequence(inputs, batch_first=True)
-        else:
-            inputs = pad_sequence(inputs,
-                                  batch_first=True,
-                                  padding_value=self.pad_token_id)
-        return inputs
-
-
-def create_line_by_line_dataset(tokenizer, paths):
-    datasets = [LineByLineDataset(tokenizer, p) for p in paths]
-    dataset = ConcatDataset(datasets)
-    return dataset
-
-
-class LineByLineDataset(Dataset):
-    def __init__(self, tokenizer, path):
-        self.path = path
-        with open(path) as f:
-            self.len = len(f.readlines())
-        self.lines = None
+        self.block_size = block_size
 
     def __len__(self):
-        return self.len
+        return len(self.lines)
 
-    def __getitem__(self, i):
-        if not self.lines:
-            with open(self.path) as f:
-                self.lines = f.readlines()
-        item = self.lines[i]
-        if i == len(self.lines):
-            self.lines = None
-
-        return item
+    def __getitem__(self, i) -> torch.Tensor:
+        return self.tokenizer.encode(
+            self.lines[i],
+            add_special_tokens=True,
+            truncation=True,
+            max_length=self.block_size,
+            return_tensors="pt",
+        ).squeeze()
