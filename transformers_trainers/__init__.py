dfef73bffc1deab504f047c9ca35750d9801baee
Sho Arora <shoarora@mercari.com>
2020-08-13 14:37:47 -0400

    Fix import

--- transformers_trainers/__init__.py
+++ transformers_trainers/__init__.py
@@ -1,8 +1,8 @@
 """transformers_trainers - Tools for training pytorch language models"""
-from .datasets import LineByLineDataset
+from .datasets import LineByLineTextDataset
 from .lightning_modules import ElectraTrainer, ElectraTrainerConfig
 
 
 __version__ = "0.2.0"
 __author__ = "Sho Arora <shoarora@cs.stanford.edu>"
-__all__ = [LineByLineDataset, ElectraTrainer, ElectraTrainerConfig]
+__all__ = [LineByLineTextDataset, ElectraTrainer, ElectraTrainerConfig]
