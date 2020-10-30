d7466f37527227a1e02a968565cbb7e57f990b76
Sho Arora <shoarora@mercari.com>
2020-10-30 15:17:20 -0700

    Remove layer sharing callback

--- transformers_trainers/callbacks/__init__.py
+++ transformers_trainers/callbacks/__init__.py
@@ -1,4 +1,3 @@
 from .hf_model_save import HFModelSaveCallback
-from .electra_layer_sharing import ElectraLayerSharingCallback
 
-__all__ = [HFModelSaveCallback, ElectraLayerSharingCallback]
+__all__ = [HFModelSaveCallback]
