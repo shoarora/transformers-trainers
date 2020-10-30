1bcb3fd278de5270ed764589f525d837f3fbefde
Sho Arora <shoarora@mercari.com>
2020-10-30 15:15:21 -0700

    Implement layer sharing callback

---
+++ experiments/v0.2.0/electra-shared-layers/train.py
@@ -0,0 +1,83 @@
+import os
+
+import datasets
+import hydra
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from transformers import (AutoTokenizer, DataCollatorForLanguageModeling,
+                          ElectraConfig, ElectraForMaskedLM,
+                          ElectraForTokenClassification)
+from transformers_trainers import ElectraTrainer, ElectraTrainerConfig
+from transformers_trainers.callbacks import ElectraLayerSharingCallback
+from transformers_trainers.datasets import NlpWrapperDataset
+from transformers_trainers.utils.logging import (WandbCheckpointCallback,
+                                                 get_logger_and_ckpt_path)
+
+CONFIG_PATH = "config/config.yaml"
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+
+@hydra.main(
+    config_path=CONFIG_PATH, strict=False,
+)
+def train(cfg):
+    print(cfg.pretty())
+    cfg.model.discriminator_name = os.path.join(
+        CWD, "model_configs", cfg.model.discriminator_name + ".json"
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(cfg.model.tokenizer_path, use_fast=True)
+    g_config = ElectraConfig.from_pretrained(cfg.model.discriminator_name)
+    d_config = ElectraConfig.from_pretrained(cfg.model.discriminator_name)
+
+    # g_config.vocab_size = tokenizer.vocab_size
+    # d_config.vocab_size = tokenizer.vocab_size
+
+    generator = ElectraForMaskedLM(g_config)
+    discriminator = ElectraForTokenClassification(d_config)
+
+    train_cfg = ElectraTrainerConfig(**cfg.model.training)
+    lightning_module = ElectraTrainer(generator, discriminator, tokenizer, train_cfg)
+
+    train_loader, val_loader = get_dataloaders(tokenizer, cfg.data)
+
+    callbacks = [ElectraLayerSharingCallback(lightning_module, **cfg.layer_sharing)]
+    if cfg.logger.type == "wandb":
+        callbacks.append(WandbCheckpointCallback())
+
+    logger, ckpt_path = get_logger_and_ckpt_path(cfg.logger)
+    trainer = pl.Trainer(
+        callbacks=callbacks,
+        logger=logger,
+        resume_from_checkpoint=ckpt_path,
+        **cfg.trainer
+    )
+    trainer.fit(lightning_module, train_loader, val_loader)
+
+
+def get_dataloaders(tokenizer, cfg):
+    dataset = datasets.load_dataset(
+        cfg.dataset_path, cfg.dataset_version, split=datasets.Split.TRAIN
+    )
+    print(dataset.features)
+    dataset.set_format(columns=[cfg.column])
+
+    dataset = NlpWrapperDataset(
+        dataset, tokenizer, cfg.column, cfg.block_size, cfg.pretokenize
+    )
+
+    collater = DataCollatorForLanguageModeling(
+        tokenizer, mlm=True, mlm_probability=cfg.mlm_probability
+    )
+    train_loader = DataLoader(
+        dataset,
+        batch_size=cfg.batch_size,
+        collate_fn=collater,
+        num_workers=cfg.num_workers,
+    )
+
+    return train_loader, train_loader
+
+
+if __name__ == "__main__":
+    train()
--- transformers_trainers/callbacks/electra_layer_sharing.py
+++ transformers_trainers/callbacks/electra_layer_sharing.py
@@ -1,21 +1,27 @@
 import pytorch_lightning as pl
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ElectraLayerSharingCallback(pl.Callback):
-    def __init__(self, pl_module, starting_num_layers=3, layer_add_rate=10e5):
+    def __init__(self, pl_module, starting_num_layers=3, layer_add_rate=5e4):
         self.layer_add_rate = layer_add_rate
         self.starting_num_layers = starting_num_layers
         self.last_update = 0
 
         self.cur_layers = starting_num_layers
-        pl_module.generator.base_model.layers = pl_module.discriminator.base_model.layers[
-            self.cur_layers
-        ]
+        self.assign_layers(pl_module.generator, pl_module.discriminator, self.cur_layers)
+        self.do_update = True
 
     def on_validation_end(self, trainer, pl_module):
-        if trainer.global_step > self.last_update + self.layer_add_rate:
-            pl_module.generator.base_model.layers = pl_module.discriminator.base_model.layers[
-                self.cur_layers + 1
-            ]
+        if trainer.global_step > self.last_update + self.layer_add_rate and self.do_update:
+            self.assign_layers(pl_module.generator, pl_module.discriminator, self.cur_layers + 1)
             self.cur_layers += 1
             self.last_update += self.layer_add_rate
+
+        self.do_update = self.cur_layers < len(pl_module.discriminator.base_model.encoder.layer)
+
+    def assign_layers(self, g, d, num_layers):
+        g.base_model.encoder.layer = d.base_model.encoder.layer[:num_layers]
+        logger.info(f"Setting generator to use the first {num_layers} layers of the discriminator")
