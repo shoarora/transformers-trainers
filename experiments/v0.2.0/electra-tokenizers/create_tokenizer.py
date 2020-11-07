78cb1b61cd19b2fe319204f47fe89b90f7d755c7
Sho Arora <shoarora@mercari.com>
2020-11-06 16:46:10 -0800

    Fix newline placement

--- experiments/v0.2.0/electra-tokenizers/create_tokenizer.py
+++ experiments/v0.2.0/electra-tokenizers/create_tokenizer.py
@@ -12,7 +12,7 @@ def create_tokenizer(dataset_name, dataset_version, dataset_column, tokenizer_ty
 
     filename = tempfile.mkstemp()[1]
     f = open(filename, "w")
-    dataset.map(lambda x: f.write(x[dataset_column + "\n"]))
+    dataset.map(lambda x: f.write(x[dataset_column] + "\n"))
     f.close()
 
     if tokenizer_type == "bpe":
