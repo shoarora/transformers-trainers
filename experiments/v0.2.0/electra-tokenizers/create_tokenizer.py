271861dea73418ac911348d2ae026f96e14eed23
Sho Arora <shoarora@mercari.com>
2020-11-10 17:29:56 -0800

    Write dataset to tempdir instead of tempfile

--- experiments/v0.2.0/electra-tokenizers/create_tokenizer.py
+++ experiments/v0.2.0/electra-tokenizers/create_tokenizer.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 
 import datasets
@@ -10,14 +11,7 @@ def create_tokenizer(dataset_name, dataset_version, dataset_column, tokenizer_ty
     dataset = datasets.load_dataset(dataset_name, dataset_version, split=datasets.Split.TRAIN)
     dataset.set_format(columns=[dataset_column])
 
-    filename = tempfile.mkstemp()[1]
-    f = open(filename, "w")
-
-    def write_line(x):
-        f.write(x[dataset_column] + "\n")
-
-    dataset.map(write_line)
-    f.close()
+    dataset_paths = write_dataset_to_tempfile(dataset, dataset_column)
 
     if tokenizer_type == "bpe":
         tokenizer = tokenizers.ByteLevelBPETokenizer()
@@ -28,8 +22,27 @@ def create_tokenizer(dataset_name, dataset_version, dataset_column, tokenizer_ty
     else:
         raise Exception(f"Tokenizer type {tokenizer_type} unsupported.")
 
-    tokenizer.train(files=filename, vocab_size=vocab_size)
+    tokenizer.train(files=dataset_paths, vocab_size=vocab_size)
     tokenizer.save(output_path)
+    print("saved tokenizer to", output_path)
+
+
+def write_dataset_to_tempfile(dataset: datasets.Dataset, dataset_column: str, num_shards=32):
+    dirname = tempfile.mkdtemp()
+
+    file_paths = [os.path.join(dirname, f"{i}.txt") for i in range(num_shards)]
+    file_handles = [open(path) for path in file_paths]
+
+    def write_line(x):
+        i = len(x[dataset_column]) % num_shards
+        file_handles[i].write(x[dataset_column] + "\n")
+
+    dataset.map(write_line)
+
+    for f in file_handles:
+        f.close()
+
+    return file_paths
 
 
 if __name__ == "__main__":
